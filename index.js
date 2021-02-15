fc4ea1a32d61e054b7aa4d2c1f7e7c6c5539977f
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-02-15 08:38:25 -0500

    firsT

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Toolkit } = require('actions-toolkit')
+
+// Run your GitHub Action!
+Toolkit.on('issue_comment.created', async context => {
+  // return if you ain't suppose be here
+  if (context.payload.comment.body.includes('.gipht')) {
+
+    const searchTerm = context.payload.comment.body.split(".gipht").join(" ")
+
+    const url = `http://api.giphy.com/v1/gifs/translate?api_key=${giphyAPIKey}&s=${searchTerm}`
+    const gifURL = await fetchGif(url)
+
+    const params = context.issue({body: `![](${gifURL})` })
+
+    return context.github.issues.createComment(params)
+
+  }
+})
+
+Toolkit.run(async tools => {
+  tools.exit.success('We did it!')
+})
