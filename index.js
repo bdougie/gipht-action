a25c01e680e92c57ca2f1263de5750abe7ef9d15
Brian Douglas <bdougie@users.noreply.github.com>
2021-02-15 08:59:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,18 @@ const { Toolkit } = require('actions-toolkit')
 const giphyAPIKey = process.env.GIPHY_TOKEN
 
 // Run your GitHub Action!
-Toolkit.on('issue_comment.created', async context => {
+Toolkit.run(async tools => {
   // return if you ain't suppose be here
-  if (context.payload.comment.body.includes('.gipht')) {
+  if (tools.context.payload.comment.body.includes('.gipht')) {
 
-    const searchTerm = context.payload.comment.body.split(".gipht").join(" ")
+    const searchTerm = tools.context.payload.comment.body.split(".gipht").join(" ")
 
     const url = `http://api.giphy.com/v1/gifs/translate?api_key=${giphyAPIKey}&s=${searchTerm}`
     const gifURL = await fetchGif(url)
 
-    const params = context.issue({body: `![](${gifURL})` })
+    const params = tools.context.issue({body: `![](${gifURL})` })
 
-    return context.github.issues.createComment(params)
+    return tools.context.github.issues.createComment(params)
 
   }
-})
-
-Toolkit.run(async tools => {
-  tools.exit.success('We did it!')
-})
+}, {event: 'issue_comment.created' })
